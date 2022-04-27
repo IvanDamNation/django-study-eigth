f2e3cb07e37a037129a59bb576d61f2835cab24f
IvanDamNation <ivan.suvalkin@gmail.com>
2022-04-27 07:53:24 +0300

    Celery-redi setup

--- NewsPaper/celery.py
+++ NewsPaper/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTNGS_MODULE', 'NewsPaper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 
 app = Celery('NewsPaper')
 
--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,4 +1,6 @@
+from NewsPaper.celery import app
 
 
+@app.task
 def send_mails():
     print('Hello from background task!')
