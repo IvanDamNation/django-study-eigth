affa1e690e252abcbd2e335ce2112a7626c98df8
IvanDamNation <ivan.suvalkin@gmail.com>
2022-05-11 23:45:17 +0300

    Add cache

--- news/urls.py
+++ news/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from accounts.views import add_subscribe
 from .views import PostList, PostDetail, PostSearch, PostAdd, PostUpdateView, PostDeleteView
 
+from django.views.decorators.cache import cache_page
+
 
 urlpatterns = [
     path('', PostList.as_view()),
-    path('<int:pk>', PostDetail.as_view(), name='PostDetail'),
+    path('<int:pk>', cache_page(60*5)(PostDetail.as_view()), name='PostDetail'),
     path('search/', PostSearch.as_view(), name='NewsSearch'),
     path('add/', PostAdd.as_view(), name='NewsAdd'),
     path('<int:pk>/edit/', PostUpdateView.as_view(), name='NewsEdit'),
