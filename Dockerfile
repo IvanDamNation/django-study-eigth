d4625ef6a7cc4261a1b1de724ab346ff17689708
Semyon <b-semen-b@mail.ru>
2022-06-23 21:36:39 +0300

    Hotfix task send mail scheduler

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3
 ENV PYTHONUNBUFFERED=1
-WORKDIR /usr/src/app
+WORKDIR /app
 COPY requirements.txt ./
+EXPOSE 8018
+COPY . /app
 RUN pip install -r requirements.txt
--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,11 +1,11 @@
 from celery import shared_task
-from accounts.management.commands.runapscheduler import news_sender
+from accounts.utils import news_sender
 
-
-@shared_task
-def send_mails():
-    print('Hello from background task!')
-
+#
+# @shared_task
+# def send_mails():
+#     print('Hello from background task!')
+#
 
 @shared_task
 def send_mail_for_sub_every_week():
